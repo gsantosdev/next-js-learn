839625e447cebd60eed27633b7f233c35c040387
santo <santosgustavo431@gmail.com>
2023-04-01 12:42:37 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,14 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <h1>QUERO MORDER PRECISO MORDER</h1>
-        
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
         <div className={styles.thirteen}>
           <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
         </div>
